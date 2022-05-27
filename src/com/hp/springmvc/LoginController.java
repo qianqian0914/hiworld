51af1c2fe6980c067967db68881ccb08bba0691d
qianqian <2735238059@qq.com>
2022-05-27 13:26:25 +0800

    提交测试 2022-05-27

---
+++ src/com/hp/springmvc/LoginController.java
@@ -0,0 +1,11 @@
+package com.hp.springmvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+@Controller
+public class LoginController {
+    @RequestMapping("/login")
+    public String toLoginPage(){
+        return "login";
+    }
+}
